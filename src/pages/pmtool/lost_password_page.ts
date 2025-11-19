3b6ec1b3ebb8c6c0432df14fd8059cdf414c693b
Petr Fifka <petr.fifka@tredgate.cz>
2025-11-19 20:40:45 +0100

    Add fluent interface, migrate page objects, add new tests with fluent

--- src/pages/pmtool/lost_password_page.ts
+++ src/pages/pmtool/lost_password_page.ts
@@ -1,10 +1,12 @@
 import { Locator, Page } from "@playwright/test";
+import { LoginPage } from "./login_page.ts";
 
 export class LostPasswordPage {
   readonly page: Page;
   readonly usernameInput: Locator;
   readonly emailInput: Locator;
   readonly sendButton: Locator;
+  readonly backButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -15,17 +17,26 @@ export class LostPasswordPage {
       ":nth-child(3) > .input-icon > .form-control"
     );
     this.sendButton = page.locator(".btn-info");
+    this.backButton = page.locator("#back-btn");
   }
 
   async fillUsername(username: string) {
     await this.usernameInput.fill(username);
+    return this;
   }
 
   async fillEmail(email: string) {
     await this.emailInput.fill(email);
+    return this;
   }
 
   async clickSend() {
     await this.sendButton.click();
+    return new LoginPage(this.page);
+  }
+
+  async clickBack() {
+    await this.backButton.click();
+    return new LoginPage(this.page);
   }
 }
