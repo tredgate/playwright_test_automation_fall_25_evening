90afca79237494cbceafc32113ada935b3c7a4f1
Petr Fifka <petr.fifka@tredgate.cz>
2025-12-09 19:24:00 +0100

    Add data driven tests, dictionaries, .env examples

---
+++ tests/learning/dictionary/dictionaries.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "@playwright/test";
+import { LoginPage } from "../../../src/pages/pmtool/login_page.ts";
+import { pmtoolTexts } from "../../../assets/dictionaries/dictionary";
+
+test("Using dictionaries to Reuse Texts", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.open();
+  await expect(page.locator(".form-title")).toHaveText(pmtoolTexts.login.title);
+  await expect(page.locator("#username")).toHaveAttribute(
+    "placeholder",
+    pmtoolTexts.login.usernamePlaceholder
+  );
+});
